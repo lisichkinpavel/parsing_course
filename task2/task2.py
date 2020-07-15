24f8ab97d17ac2622076a81fa262b32bc60386ac
Pavel Lisichkin <Pavel.lisichkin@gmail.com>
2020-07-16 00:45:12 +0300

    Add files via upload

---
+++ task2/task2.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+key = 'Iauj4k413w8jnan7xykibj4yVUbrcVBSE19abBGY'
+service = 'https://api.nasa.gov/planetary/apod'     # service provide Astronomy Picture of the Day
+parameters = {
+    'date': '2020-07-10',
+    'hd': False,
+    'api_key': key
+}
+
+responce = requests.get(service, params=parameters)
+if responce.ok:
+    data = responce.json()
+    pic_title = f"{data['title']}.jpg"
+    pic_url = data['url']
+    with open(pic_title, 'wb') as f:                 # save picture
+        f.write(requests.get(pic_url).content)
+    with open('responce_out.json', 'w') as f:        # save responce
+        json.dump(data, f, indent=2)
